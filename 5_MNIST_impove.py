aaf1f7ca76ffd6a25722c85acf7da358f4a664ef
Quanwei <1320321319@qq.com>
2019-01-15 09:26:12 +0800

    Update 5_MNIST_impove.py

--- 5_MNIST_impove.py
+++ 5_MNIST_impove.py
@@ -1,38 +1,89 @@
-#!/usr/bin/python
-# coding:utf-8
-
-import tensorflow as tf
+# load MNIST data
 import input_data
-# 加载数据
-mnist = input_data.read_data_sets('Mnist_data', one_hot=True)
 
-# 用占位符定义输入图片x与输出类别y_
-x = tf.placeholder("float", shape=[None, 784])
-y_ = tf.placeholder("float", shape=[None, 10])
-# 将权重W和偏置b定义为变量,并初始化为0向量
+mnist = input_data.read_data_sets("Mnist_data/", one_hot=True)
+
+# start tensorflow interactiveSession
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+
+# weight initialization
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+# convolution
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+# pooling
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+# Create the model
+# placeholder
+x = tf.placeholder("float", [None, 784])
+y_ = tf.placeholder("float", [None, 10])
+# variables
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-# 类别预测与损失函数
+
 y = tf.nn.softmax(tf.matmul(x, W) + b)
-# 交叉熵损失函数
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-# 训练模型
-# 用最速下降法让交叉熵下降,步长为0.01
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
-# 运行交互计算图
-sess = tf.InteractiveSession()
-# 变量初始化
-sess.run(tf.initialize_all_variables())
-# 每次加载50个训练样本,然后执行一次train_step,通过feed_dict将x和y_用训练训练数据替代
-for i in range(1000):
-    # 每次加载50个样本,返回一个tuple,元素1为样本,元素2为标签
-    batch = mnist.train.next_batch(50)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+# first convolutinal layer
+w_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
 
-# 评估模型
-# 用tf.equal来检测预测是与否真实标签匹配
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-# 将布尔值转换为浮点数来代表对错然后取平均值
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+# second convolutional layer
+w_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# densely connected layer
+w_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
+
+# dropout
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# readout layer
+w_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+
+# train and evaluate the model
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
+train_step = tf.train.AdagradOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print accuracy.eval(feed_dict={x: mnist.test.images, y_:mnist.test.labels})
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, train accuracy %g" % (i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
